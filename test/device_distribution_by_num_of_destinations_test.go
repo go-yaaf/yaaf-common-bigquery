d10b23967f62d16dcdcf0c8ef41cacaab1caf223
glantsalex <glants.alex@gmail.com>
2025-01-29 16:56:49 +0200

    work in progress

--- test/device_distribution_by_num_of_destinations_test.go
+++ test/device_distribution_by_num_of_destinations_test.go
@@ -12,35 +12,37 @@ import (
 	"github.com/go-yaaf/yaaf-common/logger"
 )
 
-type NewtworkActivityOverTime struct {
+type DeviceDestinationDistribution struct {
 	BaseAnalyticEntity
-	StreamId     string `json:"-"`
-	TimePoint    int64  `json:"timestamp" bq:"start_time"`
-	NumOfDevices int64  `json:"value"     bq:"device_id"`
+	StreamId   string `json:"-"`
+	TimePoint  int64  `json:"-"          bq:"start_time"`
+	DeviceId   string `json:"deviceId"   bq:"device_id"`
+	NumOfDstIp int64  `json:"numOdDstIp" bq:"dst_ip"`
 }
 
-func (f *NewtworkActivityOverTime) KEY() string {
+func (f *DeviceDestinationDistribution) KEY() string {
 	return f.StreamId
 }
 
-func (f *NewtworkActivityOverTime) TABLE() string {
+func (f *DeviceDestinationDistribution) TABLE() string {
 	return fmt.Sprintf("flow-data-%s", f.KEY())
 }
 
-func NewNetworkActivityOverTime(shardKey string) entity.EntityFactory {
+func NewDeviceDestinationDistribution(shardKey string) entity.EntityFactory {
 	return func() entity.Entity {
 		shardKey := shardKey
-		return &NewtworkActivityOverTime{
+		return &DeviceDestinationDistribution{
 			StreamId: shardKey,
 		}
 	}
 }
-func TestNetworkActivityOverTime(t *testing.T) {
+
+func Test_Device_Distribution_By_Num_Of_Destinations(t *testing.T) {
 	if os.Getenv("CI") != "" {
 		t.Skip("Skipping testing in CI environment")
 	}
 	shardKey := "etecnic-1"
-	factory := NewNetworkActivityOverTime(shardKey)
+	factory := NewDeviceDestinationDistribution(shardKey)
 
 	bqdb, err := bigquerydb.NewBqDatabase("bq://shieldiot-staging:pulseiot")
 
@@ -50,10 +52,10 @@ func TestNetworkActivityOverTime(t *testing.T) {
 
 	qa := bqdb.AdvancedQuery(factory)
 	qa.Filter(F("start_time").Between(1735776000000, 1736467200000)).
-		Sort("timestamp-")
+		Sort("device_id-")
 
-	entities, err := qa.GroupBy("start_time", entity.TimePeriodCodes.HOUR).
-		CountUnique("device_id").
+	entities, err := qa.GroupBy("device_id", entity.TimePeriodCodes.UNDEFINED).
+		CountUnique("dst_ip").
 		Compute()
 
 	if err != nil {
@@ -69,5 +71,4 @@ func TestNetworkActivityOverTime(t *testing.T) {
 	// Print the result
 	fmt.Println(string(jsonData))
 	logger.Debug("count: %d", len(entities))
-
 }
